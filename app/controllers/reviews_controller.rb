9f1749cbc74b8408477942324e73ff4e4fbb32f9
Hui Yong Teng <tenghuiyong@outlook.sg>
2020-11-26 00:16:59 +0800

    yelp mvp

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -6,7 +6,8 @@ class ReviewsController < ApplicationController
 
   def create
     @restaurant = Restaurant.find(params[:restaurant_id])
-    @review = Review.new(params[:review])
+    @review = Review.new(review_params)
+    @review.restaurant = @restaurant
     if @review.save
       redirect_to restaurant_path(@restaurant)
     else
